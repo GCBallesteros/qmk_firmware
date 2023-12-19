b0c853e34c4b8ccf372e0cdacf2bd14be0d333da
GCBallesteros <dev+github@maxwellrules.com>
2023-12-19 21:57:17 +0100

    feat: Add Q11 personal config with lite home row mod

---
+++ keyboards/keychron/q11/ansi_encoder/keymaps/GCBallesteros/config.h
@@ -0,0 +1,7 @@
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
+
+// Only bilateral combinations count for mod-tap unless they
+// are held for longer than 400 ms. Avoids missfires of mod
+// on rolls
+#define BILATERAL_COMBINATIONS 400
