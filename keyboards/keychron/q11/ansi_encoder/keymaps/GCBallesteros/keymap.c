1bcea49582d6ae5395bac2f480ab3b9a0ea69639
GCBallesteros <dev+github@maxwellrules.com>
2024-06-02 21:44:59 +0200

    Move calculator

--- keyboards/keychron/q11/ansi_encoder/keymaps/GCBallesteros/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/GCBallesteros/keymap.c
@@ -29,6 +29,9 @@
 // Toggle layer 2 or tab
 #define L2_TAB LT(2, KC_TAB)
 
+// Calculator or ESC at CapsLock position
+#define ESC_CA LT(3, KC_ESC)
+
 // Extra keycodes
 // defined on: keychron/common/keychron_common.h
 // - VIMEND (g_)
@@ -55,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
         MC_1,     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  XXXXXXX,  XXXXXXX,            KC_PGUP,
-        MC_2,     MO(_NUM), KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  XXXXXXX,            KC_PGDN,
-        MC_3,     KC_ESC,   KC_A,     KC_S,     HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   KC_L,     KC_SCLN,  KC_QUOT,            XXXXXXX,            KC_HOME,
+        MC_2,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  XXXXXXX,            KC_PGDN,
+        MC_3,     ESC_CA,   KC_A,     KC_S,     HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   KC_L,     KC_SCLN,  KC_QUOT,            XXXXXXX,            KC_HOME,
         MC_4,     KC_ENT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            XXXXXXX,  KC_UP,
         MC_5,     XXXXXXX,  KC_LOPTN, KC_LCMMD, MO(_NAV),           L2_TAB,                       KC_SPC,             KC_RCMMD, MO(_NUM), KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
